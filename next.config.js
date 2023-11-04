3a9d752fe8222b2f51c09680dc65048d072e77ee
viruop <virajrai706@gmail.com>
2023-11-04 13:47:23 +0530

    wrong format

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
-  images: ["resend.com"],
+  images: { domains: ["resend.com"] },
 };
 
 module.exports = nextConfig;
